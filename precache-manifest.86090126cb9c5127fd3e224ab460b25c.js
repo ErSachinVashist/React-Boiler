536ca0c427fed2620a4b360198084d18eb84ee97
Vashist <sachin.v@serverguy.com>
2019-02-21 12:49:34 +0530

    Updates

---
+++ precache-manifest.86090126cb9c5127fd3e224ab460b25c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1716baa09161da5443ec49c071921c42",
+    "url": "/react-boiler/static/media/reactHome.1716baa0.png"
+  },
+  {
+    "revision": "e614ca1673660c1d59db8a80809b48bc",
+    "url": "/react-boiler/static/media/profile.e614ca16.jpg"
+  },
+  {
+    "revision": "b596bf2731eda80d1d6a",
+    "url": "/react-boiler/static/js/runtime~main.b596bf27.js"
+  },
+  {
+    "revision": "9122474ef335dbd47471",
+    "url": "/react-boiler/static/js/main.9122474e.chunk.js"
+  },
+  {
+    "revision": "02c830cce70b05c19b34",
+    "url": "/react-boiler/static/js/1.02c830cc.chunk.js"
+  },
+  {
+    "revision": "9122474ef335dbd47471",
+    "url": "/react-boiler/static/css/main.cd919bff.chunk.css"
+  },
+  {
+    "revision": "cd4745e1d91111ad6b297fbd96912413",
+    "url": "/react-boiler/index.html"
+  }
+];
